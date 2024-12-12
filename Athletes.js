7d51babeea0cb261ca91a444febee208bd2f4082
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 10:26:29 +0100

    Update Athletes.js

--- Athletes.js
+++ Athletes.js
@@ -1,22 +1,22 @@
 function solve(athletes) {
-    // Filter athletes by sport
+   
     function getAthletesBySport(sport) {
         return athletes.filter(athlete => athlete.sport === sport);
     }
 
-    // Add a new athlete
+ 
     function addAthlete(id, name, sport, medals, country) {
         athletes.push({ id, name, sport, medals, country });
         return athletes;
     }
 
-    // Find athlete by ID
+
     function getAthleteById(id) {
         const athlete = athletes.find(athlete => athlete.id === id);
         return athlete ? athlete : `Athlete with ID ${id} not found`;
     }
 
-    // Remove athlete by ID
+  
     function removeAthleteById(id) {
         const index = athletes.findIndex(athlete => athlete.id === id);
         if (index !== -1) {
@@ -26,7 +26,7 @@ function solve(athletes) {
         return `Athlete with ID ${id} not found`;
     }
 
-    // Update athlete's medal count
+   
     function updateAthleteMedals(id, newMedals) {
         const athlete = athletes.find(athlete => athlete.id === id);
         if (athlete) {
@@ -36,7 +36,7 @@ function solve(athletes) {
         return `Athlete with ID ${id} not found`;
     }
 
-    // Update athlete's country
+  
     function updateAthleteCountry(id, newCountry) {
         const athlete = athletes.find(athlete => athlete.id === id);
         if (athlete) {
@@ -46,7 +46,7 @@ function solve(athletes) {
         return `Athlete with ID ${id} not found`;
     }
 
-    // Return an object with references to all the methods
+ 
     return {
         getAthletesBySport,
         addAthlete,
